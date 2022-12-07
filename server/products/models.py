87860153903637a00c7b88cc9224f681af987a55
Zobaer <zzobaer09@gmail.com>
2022-12-07 22:00:13 +0600

    databese migrated

--- server/products/models.py
+++ server/products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255 , null=False , blank=False)
     price = models.DecimalField(max_digits=7 , decimal_places=2, null=False, blank=False)
     description = models.TextField()
-    cetegory = models.CharField(max_length=255 , null=True, blank=True)
+    category = models.CharField(max_length=255 , null=True, blank=True)
 
     def __str__(self):
         return self.name
